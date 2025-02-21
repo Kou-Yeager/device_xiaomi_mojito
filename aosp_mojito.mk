bfbb5803f4cf4b6ca0ab3b3a099720a0d10f9b4e
Kou-Yeager <wongical552@gmail.com>
2025-02-21 21:13:04 +0700

    mojito: bring up @PixelExperience-LEGACY-edition

--- aosp_mojito.mk
+++ aosp_mojito.mk
@@ -12,16 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full.mk)
 
 # Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_SUPPORTS_QUICK_TAP := true
 
-TARGET_USES_AOSP_RECOVERY := true
-
-PRODUCT_NAME := lineage_mojito
+PRODUCT_NAME := aosp_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
